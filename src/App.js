4f9200f753de0e74e404b0673bcd6eb42ce7d2f8
Kartik kumar <kumarkartik147359@gmail.com>
2025-02-18 20:24:59 +0530

    Initial Commit2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    window.location.href = "https://beaconator.com";
+  }, []);
+
   return (
     <div className="App">
     </div>
